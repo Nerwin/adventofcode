36fca343bbf2d5ee71165a0dee20404cb829ece4
Nerwin <nerwindev@gmail.com>
2020-12-04 15:14:20 -0500

    little cleaning

--- days/04/index.js
+++ days/04/index.js
@@ -6,23 +6,20 @@ import fs from 'fs';
  * @description Count the number of valid passports - those that have all required fields and valid values
  */
 
-const inRange = (value, min, max) => value >= min && value <= max;
+const isInRange = (value, min, max) => value >= min && value <= max;
+
 const fieldValidator = {
-  byr: { validate: (value) => inRange(value, 1920, 2002) },
-  iyr: { validate: (value) => inRange(value, 2010, 2020) },
-  eyr: { validate: (value) => inRange(value, 2020, 2030) },
+  byr: { validate: (value) => isInRange(value, 1920, 2002) },
+  iyr: { validate: (value) => isInRange(value, 2010, 2020) },
+  eyr: { validate: (value) => isInRange(value, 2020, 2030) },
+  hcl: { validate: (value) => value.search(/#[\da-f]{6}/) !== -1 },
+  ecl: { validate: (value) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value) },
+  pid: { validate: (value) => !isNaN(value) && value.length === 9 },
   hgt: {
     validate: (value) => {
       const [_match, size, metric] = value.match(/(\d{2,3})(cm|in)/) || [];
-      return (metric === 'cm' && inRange(size, 150, 193)) || (metric === 'in' && inRange(size, 59, 76));
+      return (metric === 'cm' && isInRange(size, 150, 193)) || (metric === 'in' && isInRange(size, 59, 76));
     },
-  },
-  hcl: {
-    validate: (value) => value.search(/#[\da-f]{6}/) !== -1,
-  },
-  ecl: { validate: (value) => ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value) },
-  pid: {
-    validate: (value) => !isNaN(value) && value.length === 9,
   },
 };
 
@@ -31,9 +28,8 @@ const day04 = () => {
 
   const result = passports.filter((passport) => {
     return Object.keys(fieldValidator).every((fieldName) => {
-      const match = passport.match(new RegExp(`${fieldName}:(#?\\w+)`));
-      if (!match) return false;
-      return fieldValidator[fieldName].validate(match[1]);
+      const matches = passport.match(new RegExp(`${fieldName}:(#?\\w+)`));
+      return matches && fieldValidator[fieldName].validate(matches[1]);
     });
   }).length;
 
